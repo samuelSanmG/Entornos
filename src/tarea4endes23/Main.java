8d1943d55a070fe37a9a24e23ab9107a9c403d58
samuel <samuelsanmiguel9@gmail.com>
2024-04-25 19:38:11 +0200

    Primer commit

---
+++ src/tarea4endes23/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tarea4endes23;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta miCuenta;
+        double saldoActual;
+        
+
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        
+    
+    }
+    
+    public static void probarIngresar(CCuenta c, int cantidad) throws Exception{
+        c.ingresar(cantidad);
+        
+    }
+    
+    public static void probarRetirar(CCuenta c, int cantidad) throws Exception{
+        c.retirar(cantidad);
+    }
+}
